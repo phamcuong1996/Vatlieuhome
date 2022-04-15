1af2b040462ab68abfb9808c2a773bbafc7d1011
phammanhcuong1996 <you@example.com>
2022-04-15 16:56:36 +0700

    Edit migration

--- database/migrations/2022_04_15_095250_create_post_categories_table.php
+++ database/migrations/2022_04_15_095250_create_post_categories_table.php
@@ -15,7 +15,12 @@ class CreatePostCategoriesTable extends Migration
     {
         Schema::create('post_categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->softDeletes();
         });
     }
 
